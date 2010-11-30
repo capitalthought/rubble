b064f15fedc87d3a5b78311aa27501e92703c2cf
shock <billdoughty@capitalthought.com>
2010-11-30 16:57:25 -0600

    Include RubbleHelper in ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
+  # include helpers for the the Rubble framework
   def self.included(base)
-    base.include RubbleHelper
+    base.send(:include, RubbleHelper)
   end
 end
