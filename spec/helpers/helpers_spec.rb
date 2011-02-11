2e1f35be9ec5e6187614be6bae16f803c062d0a6
Robert Eanes <github@robsinbox.com>
2011-02-11 17:09:27 -0600

    finishes specs for helper methods

--- spec/helpers/helpers_spec.rb
+++ spec/helpers/helpers_spec.rb
@@ -6,14 +6,55 @@ describe "Rubble::Helpers" do
   before(:all) do
     ApplicationHelper.send(:include, Rubble::Helpers)
   end
+  before do 
+    controller.controller_path = "controller"
+    controller.action_name = "action"
+  end
+  
+  describe "#classes_for_body" do
+    before do
+      @output = classes_for_body
+    end
+    it "should include the controller name" do
+      @output.should =~ /controller/
+    end
+    it "should include the action name" do
+      @output.should =~ /action/
+    end
+  end
   
   describe "#stylesheets_for_path" do
-    
-    it "should render stylesheet link tags for stylesheets passed in" do
-      controller.controller_path = "foo"
-      controller.action_name = "bar"
-      output = stylesheets_for_path ['foo']
-      output.should =~ /foo.css/
+    context "when the stylesheets exist" do
+      before do
+        File.stub(:exists?).and_return(true)
+        @output = stylesheets_for_path ['foo']
+      end
+      
+      it "should render stylesheet link tags for stylesheets passed in" do
+        @output.should =~ /foo.css/
+      end
+      it "should render stylesheet link tags named for the controller" do
+        @output.should =~ /controller.css/
+      end
+      it "should render stylesheet link tags named for the action" do
+        @output.should =~ /action.css/
+      end
+    end
+    context "when the stylesheets do not exist" do
+      before do
+        File.stub(:exists?).and_return(false)
+        @output = stylesheets_for_path ['foo']
+      end
+      it "should not render stylesheet link tags for stylesheets passed in" do
+        @output.should_not =~ /foo.css/
+      end
+      it "should not render stylesheet link tags named for the controller" do
+        @output.should_not =~ /controller.css/
+      end
+      it "should not render stylesheet link tags named for the action" do
+        @output.should_not =~ /action.css/
+      end
+      
     end
   end
 end
