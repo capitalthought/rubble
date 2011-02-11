af49852d37b8a9d935ae0da8b6dac1b60dd92540
Robert Eanes <github@robsinbox.com>
2011-02-11 14:23:48 -0600

    adds sass generator

---
+++ lib/generators/rubble/sass_generator.rb
@@ -0,0 +1,12 @@
+module Rubble
+  class SassGenerator < Rails::Generators::Base
+    desc "This generator copies the rubble scss files into your application"
+      
+    source_root File.expand_path('../../../../app/sass', __FILE__)
+      
+    def copy_all
+       directory '.', "rubble"
+    end
+          
+  end
+end
